037daf5b7eb18285a3f0185260aa802dd0ed7b08
Oscar <oscar.weissenbach@tu-berlin.de>
2024-07-11 17:40:17 +0200

    reworte some

--- tempserver.py
+++ tempserver.py
@@ -5,21 +5,27 @@ import asyncio
 import json
 import time
 import websockets
+import random
 
-def compute_res(x):
-    nodes = {}
-    adges = {}
-    return x+1
+
+add_or_remove = [0, 1]
+
+random.sample(add_or_remove, k=1)
+
+
+def get_activ():
+    return random.random()*100
 
 async def client_connected_handler(websocket):
     print("Client connected")
-    x = 0
     while True:
         
-        message = await websocket.send(json.dumps({"nodes": {"r": x, "activity_state": 50} , "links": {"l": [1,x-1], "activity_state": 50}}))
-        x = compute_res(x)
+        # d3js format: data = {"nodes": [{"id":1, "activity":70},{"id":2},{"id":3}], "links":[{"source":1,"target":2},{"source":2,"target":1, "activity":40}]}
+
+        data = [[0,get_activ(),2],[[0,1,get_activ()],[1,2,get_activ()]]]
+        await websocket.send(json.dumps(data))
+        # message = await websocket.send(json.dumps({"nodes": [{"id": x, "activity_state": 50}] , "links": [{"l": [1,x-1], "activity_state": 50}]}))
         time.sleep(1)
-        # print(x)
 
 
 
@@ -27,12 +33,12 @@ async def main():
     async with websockets.serve(client_connected_handler, "", 5678):
         await asyncio.Future()  # run forever
 
-def decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
-    return wrapper
+# def decorator(func):
+#     def wrapper():
+#         print("Something is happening before the function is called.")
+#         func()
+#         print("Something is happening after the function is called.")
+#     return wrapper
 
 if __name__ == "__main__":
     asyncio.run(main())
