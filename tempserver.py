cc1173936b8196bc6e615b255de21f5f81880706
oscar weissenbach <oscar.weissenbach@tu-berlin.de>
2024-09-16 12:22:22 +0200

    add in and out nodes

--- tempserver.py
+++ tempserver.py
@@ -6,12 +6,13 @@ import json
 import time
 import websockets
 import random
-
+import numpy as np
+import math
 
 add_or_remove = [0, 1]
 
 random.sample(add_or_remove, k=1)
-amount = 10
+amount = 50
 
 
 def get_activ():
@@ -21,9 +22,16 @@ async def client_connected_handler(websocket):
     print("Client connected")
     while True:
         data = [[],[]]
+        train_data = {}
+        train_data['input'] = np.ndarray(shape=(0,2))
+        train_data['output'] = np.ndarray(shape=(0,2))
+
+        
 
         nodenumber = random.sample(range(1, amount), k=1)[0]
         linknumber = random.sample(range(1, 2*amount), k=1)[0]
+        input_number = random.sample(range(1, math.floor(nodenumber/3)+2), k=1)[0]
+        output_number = random.sample(range(1, math.floor(nodenumber/3)+2), k=1)[0]
         noderange = range(0, nodenumber)
         linkrange = range(0, linknumber)
 
@@ -34,8 +42,52 @@ async def client_connected_handler(websocket):
             data[1].append([random.sample(noderange, k=1)[0],random.sample(noderange, k=1)[0],get_activ()])
         # d3js format: data = {"nodes": [{"id":1, "activity":70},{"id":2},{"id":3}], "links":[{"source":1,"target":2},{"source":2,"target":1, "activity":40}]}
 
+        for i in range(0, input_number):
+            appendix = np.array([[get_activ(),random.sample(noderange, k=1)[0]]])
+            train_data['input'] = np.append(train_data['input'], appendix, axis=0)
+            pass
+            # train_data['input'].append([get_activ(),random.sample(noderange, k=1)[0]])
+
+        for o in range(0, output_number):            
+            appendix = np.array([[get_activ(),random.sample(noderange, k=1)[0]]])
+            train_data['output'] = np.append(train_data['output'], appendix, axis=0)
+            # train_data['output'].append([get_activ(),random.sample(noderange, k=1)[0]])
+        
+
+        train_data['input'] = train_data['input'].tolist()
+        train_data['output'] = train_data['output'].tolist()
+        # train_data['input'] = np.array([
+        #     [0.7, 7],
+        #     [0, 8],
+        #     [0.5, 9],
+        #     [0.1, 10],
+        #     [0.134, 11],
+        #     [0.321, 12]
+        # ])
+
+
+        # train_data['output'] = np.array([
+        #     [0.7, 7],
+        #     [0, 8],
+        #     [0.5, 9],
+        #     [0.1, 10],
+        #     [0.134, 11],
+        #     [0.321, 12]
+        # ])
+
+        # output_mit_mapping = np.array([
+        #     [0.7, 7],
+        #     [0, 8],
+        #     [0.5, 9],
+        #     [0.1, 10],
+        #     [0.134, 11],
+        #     [0.321, 12]
+        # ])
+
+
         # data = [[0,get_activ(),2],[[0,1,get_activ()],[1,2,get_activ()]]]
         await websocket.send(json.dumps(data))
+        await websocket.send(json.dumps(train_data))
         
         time.sleep(3)
 
